508edb8d9bae27c4ba8675dd8c99206109a1e5ed
chieryW <chieryw@163.com>
2014-11-06 09:07:49 +0800

    添加工程文件

---
+++ TestCoreMotion/TestCoreMotion/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  TestCoreMotion
+//
+//  Created by chiery on 14/11/6.
+//  Copyright (c) 2014年 qunar. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ TestCoreMotion/TestCoreMotion/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  TestCoreMotion
+//
+//  Created by chiery on 14/11/6.
+//  Copyright (c) 2014年 qunar. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
